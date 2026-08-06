e070c1efa9dd76ee9ba49bf1022d5f9e72a5066b
Ivo Horak <thehoryy@gmail.com>
2026-08-06 15:23:14 +0200

    testing PRs part 2

---
+++ examples/spring/407/java-web-jar/src/test/java/com/example/java_web_jar/JavaWebJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_web_jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
